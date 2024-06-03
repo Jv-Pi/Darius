44ba35e08519a732aa7e8816916cb26f27e1cf93
João V P Santos <joaotei7@hotmail.com>
2024-06-03 10:34:10 -0400

    fim

--- js/main.js
+++ js/main.js
@@ -291,3 +291,20 @@ $(".contagem").each(function () {
       }
     );
 });
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 2,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
